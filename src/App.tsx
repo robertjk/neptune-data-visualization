79507fd563034bbe57bd7257ea5e8f2379a87079
Robert J. Kusznier <no-cheating@autistici.org>
2025-09-19 21:19:00 +0200

    Create empty Chart component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import Chart from "./Chart";
+
 import "./App.css";
 
 function App() {
   return (
     <>
       <h1>Neptune Data Visualization</h1>
+      <Chart />
     </>
   );
 }
---
+++ src/Chart/Chart.tsx
@@ -0,0 +1,71 @@
+import UplotReact from "uplot-react";
+
+import "uplot/dist/uPlot.min.css";
+
+const OPTIONS = {
+  width: 800,
+  height: 600,
+  id: "chart",
+  series: [
+    {
+      label: "X",
+    },
+    {
+      label: "Y1",
+      stroke: "blue",
+      width: 3,
+    },
+    {
+      label: "Y2",
+      stroke: "green",
+      width: 7,
+    },
+  ],
+  scales: {
+    x: {
+      time: false,
+    },
+    y: {
+      auto: true,
+    },
+  },
+  axes: [
+    {
+      show: true,
+      label: "X Axis",
+      stroke: "white",
+      grid: {
+        show: true,
+        stroke: "white",
+        width: 1,
+      },
+    },
+    {
+      show: true,
+      label: "Y Axis",
+      stroke: "white",
+      grid: {
+        show: true,
+        stroke: "white",
+        width: 1,
+      },
+    },
+  ],
+};
+
+function Chart() {
+  return (
+    <>
+      <UplotReact
+        data={[
+          [0, 1, 2, 3, 4, 5, 6],
+          [0, 1, 4, 9, 16, 25, 36],
+          [0, null, 14, null, 26, null, 50],
+        ]}
+        options={OPTIONS}
+      />
+    </>
+  );
+}
+
+export { Chart as default };
